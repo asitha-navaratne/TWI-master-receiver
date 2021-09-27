5c6f8e827d83c4174ebadcb32074ad24ef82aaf7
Asitha Navaratne <asithan476@gmail.com>
2021-09-27 22:08:41 +0530

    Justified comments

--- main.c
+++ main.c
@@ -29,8 +29,8 @@ int main(void){
 
 void TWI_START(unsigned char bitrate){
 	TWBR = 0x00;
-	TWBR = bitrate; 									///< Set the division factor of bitrate generator.
-	TWCR = (1<<TWEN)|(1<<TWINT)|(1<<TWSTA); 			///< Enable TWI and set TWINT bit to 1. Send START condition.
+	TWBR = bitrate; 							///< Set the division factor of bitrate generator.
+	TWCR = (1<<TWEN)|(1<<TWINT)|(1<<TWSTA); 				///< Enable TWI and set TWINT bit to 1. Send START condition.
 	while(!(TWCR & (1<<TWINT)));						///< Wait while TWI completes current action.
 	while((TWSR & 0xF8) != 0x08); 						///< Wait while ACK bit is received after START condition is transmitted.
 }
@@ -42,13 +42,13 @@ void TWI_START(unsigned char bitrate){
 
 void TWI_ADDRESS(unsigned char address){
 	while(1){
-		TWDR = address + 0x01; 								///< Load slave device's address and READ bit to SDA.
-		TWCR = (1<<TWEN)|(1<<TWINT);						///< Enable TWI and set TWINT bit to 1.
-		while(!(TWCR & (1<<TWINT)));						///< Wait while TWI completes current action.
+		TWDR = address + 0x01; 						///< Load slave device's address and READ bit to SDA.
+		TWCR = (1<<TWEN)|(1<<TWINT);					///< Enable TWI and set TWINT bit to 1.
+		while(!(TWCR & (1<<TWINT)));					///< Wait while TWI completes current action.
 		if((TWSR & 0xF8) != 0x40){
-			continue;										///< If ACK has not been received, repeat the loop.
+			continue;						///< If ACK has not been received, repeat the loop.
 		}
-		break;												///< If ACK has been received break from loop.
+		break;								///< If ACK has been received break from loop.
 	}
 }
 
@@ -59,13 +59,13 @@ void TWI_ADDRESS(unsigned char address){
 
 void MASTER_RECEIVE(uint8_t datasize){
 	for(uint8_t i=0;i<(datasize-1);i++){
-		TWCR = (1<<TWEN)|(1<<TWINT)|(1<<TWEA);		///< Enable TWI and set TWINT bit to 1. Generate an ACK bit on the TWI bus to call for data.
+		TWCR = (1<<TWEN)|(1<<TWINT)|(1<<TWEA);				///< Enable TWI and set TWINT bit to 1. Generate an ACK bit on the TWI bus to call for data.
 		while(!(TWCR & (1<<TWINT)));					///< Wait while TWI completes current action.
-		data[i] = TWDR; 								///< Store the data received.
+		data[i] = TWDR; 						///< Store the data received.
 	}
 	TWCR = (1<<TWEN)|(1<<TWINT);						///< When receiving last data byte generate a NACK pulse.
 	while(!(TWCR & (1<<TWINT)));						///< Wait while TWI completes current action.
-	data[datasize-1] = TWDR;							///< Store last data byte.
+	data[datasize-1] = TWDR;						///< Store last data byte.
 	TWI_STOP();
 }
 
@@ -74,5 +74,5 @@ void MASTER_RECEIVE(uint8_t datasize){
  */
 
 void TWI_STOP(void){
-	TWCR = (1<<TWEN)|(1<<TWINT)|(1<<TWSTO); 			///< Enable TWI and set TWINT bit to 1. Send STOP condition.
+	TWCR = (1<<TWEN)|(1<<TWINT)|(1<<TWSTO); 				///< Enable TWI and set TWINT bit to 1. Send STOP condition.
 }
